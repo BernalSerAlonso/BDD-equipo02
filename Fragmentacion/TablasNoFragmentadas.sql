bb41bc762aa29feaebfccb1b8f4017d700e7777c
RafaZamoraUPIITA <99270205+RafaZamoraUPIITA@users.noreply.github.com>
2022-06-21 10:26:37 -0500

    Update TablasNoFragmentadas.sql

--- Fragmentacion/TablasNoFragmentadas.sql
+++ Fragmentacion/TablasNoFragmentadas.sql
@@ -1,57 +1,34 @@
-Use Europe
+
 /*
 CREATE DATABASE Europa_Pacifico
 GO
 
 use Europa_Pacifico
 GO*/
-
+--SE USÓ PARA REPLICAR LAS TABLAS NECESARIAS PARA LAS CONSULTAS EN LOS FRAGMENTOS DE NorteAmerica y Europe
 create schema Production 
 create schema Sales
-
+--REPLICA DE SpecialOffer
 SELECT so.* INTO Sales.SpecialOffer
 FROM AdventureWorks2019.Sales.SpecialOffer so
 go
-
+--REPLICA DE SpecialOfferProduct
 SELECT sop.* INTO Sales.SpecialOfferProduct
 FROM AdventureWorks2019.Sales.SpecialOfferProduct sop
 go
-
+--REPLICA DE SalesPerson
 SELECT sp.* INTO Sales.SalesPerson
 FROM AdventureWorks2019.Sales.SalesPerson sp
 go
-
+--REPLICA DE Product
 SELECT p.* INTO Production.Product
 FROM AdventureWorks2019.Production.Product p
 go
-
+--REPLICA DE ProductCategory
 SELECT pc.* INTO Production.ProductCategory
 FROM AdventureWorks2019.Production.ProductCategory pc
 go
-
+--REPLICA DE ProductSubcategory
 SELECT psc.* INTO Production.ProductSubcategory
 FROM AdventureWorks2019.Production.ProductSubcategory psc
 go
-
-Use Europa 
--- RECONSTRUCCION
-SELECT * FROM OPENQUERY([DESKTOP-FAT50UF\BDD02],'
-Select * From [Europa].[dbo].[SalesOrderHeader] 
-UNION
-Select * FROM [Norteamerica].[dbo].[SalesOrderHeader]
-UNION
-Select * FROM [Pacifico].[dbo].[SalesOrderHeader]')
-GO
-
-SELECT * FROM SalesOrderDetail
-
-
--- DISJUNCION
-Select * From [Europa].[dbo].[SalesOrderHeader]
-INTERSECT
-Select * FROM [Norteamerica].[dbo].[SalesOrderHeader]
-INTERSECT
-SELECT * FROM [Pacifico].[dbo].[SalesOrderHeader]
-GO
-
-
