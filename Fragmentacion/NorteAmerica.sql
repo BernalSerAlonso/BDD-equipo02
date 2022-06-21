6695c1489f65d2bfa639af1bc23e9a496b64ec32
RafaZamoraUPIITA <99270205+RafaZamoraUPIITA@users.noreply.github.com>
2022-06-21 10:24:44 -0500

    Update NorteAmerica.sql

--- Fragmentacion/NorteAmerica.sql
+++ Fragmentacion/NorteAmerica.sql
@@ -1,65 +1,32 @@
-use AdventureWorks2019;
-
---TABLAS QUE REQUIEREN FRAGMENTACION DERIVADA DE CUSTOMER
---* SalesOrderHeader
---	* SalesOrderDetail
-
--- 1.1 FRAGMENTACION HORIZONTAL DE CUSTOMER
-
-Select *
-from Sales.Customer C
-where TerritoryID between 1 and 6;
---  Norte America {1,2,3,4,5,6}
-
-Select *
-from Sales.Customer C
-where TerritoryID between 7 and 9;
--- Europa {7,8,10}
-
-Select *
-from Sales.Customer C
-where TerritoryID = 10;
--- M3	-- Pacifico {10}
-
-Select *
-from Sales.Customer C
-where TerritoryID > 10;
--- M4  
-
-
--- CONSULTAS QUE GENEREN LOS FRAGMENTOS DE CUSTOMER A PARTIR DE TerritoryID
-
--- FRAGMENTOS HORIZONTALES DE SalesOrderHeader DERIVADOS DE CUSTOMER
-
+--creacion de la base de datosPARA EL FRAGMENTO NorteAmerica
 CREATE DATABASE Norteamerica
 GO
-
+--SHARDING REALIZADO EN EL SERVIDOR VINCULADO -> DESKTOP-FAT50UF\BDD01
 use Norteamerica
 GO
+-- CREAMOS LOS ESQUEMAS SALES Y PRODUCTION PARA LOS FRAGMENTOS CORRESPONDIENTES DE LAS TABLAS
 create schema Sales
+create schema Production
 
 SELECT TerritoryID
 FROM Sales.SalesTerritory
 WHERE [Group]='North America'
 
+-- FRAGMENTO DE CUSTOMER CORRESPONDIENTE A NorteAmerica -> TerritoryID=1,2,3,4,5,6
 
-SELECT c.*
-INTO Sales.Customer
+SELECT c.* INTO Sales.Customer
 FROM AdventureWorks2019.Sales.Customer c
 where TerritoryID BETWEEN 1 AND 6
-
-SELECT ORH.*
-INTO Sales.SalesOrderHeader
+--FRAGMENTO DE SalesOrderHeader CORRESPONDIENTE A [GROUP] EN SalesTerritory -> 'North America'
+SELECT ORH.* INTO Sales.SalesOrderHeader
 FROM AdventureWorks2019.Sales.SalesOrderHeader ORH
 WHERE TerritoryID in (SELECT TerritoryID
-FROM AdventureWorks2019.Sales.SalesTerritory
-WHERE [Group]='North America')
+						FROM AdventureWorks2019.Sales.SalesTerritory
+						WHERE [Group]='North America') 
 
---Fragmento SalesOrderDetail Norteamerica
-SELECT DISTINCT sod.*
-INTO Sales.SalesOrderDetail
+--Fragmento SalesOrderDetail Pacifico A PARTIR DE UN JOIN DE SalesOrderDetail CON EL FRAGMENTO DE SalesOrderHeader CORRESPONDIENTE A NorteAmerica
+SELECT DISTINCT sod.* INTO Sales.SalesOrderDetail
 FROM AdventureWorks2019.Sales.SalesOrderDetail sod
-    JOIN (SELECT *
-    FROM Norteamerica.Sales.SalesOrderHeader) st
-    ON sod.SalesOrderID = st.SalesOrderID
+JOIN (SELECT * FROM Norteamerica.Sales.SalesOrderHeader) st
+ON sod.SalesOrderID = st.SalesOrderID
 GO
