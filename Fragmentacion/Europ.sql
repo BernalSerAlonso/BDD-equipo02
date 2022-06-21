5bdf69d6e71f0a14b804422e3e0454bb1ee03785
RafaZamoraUPIITA <99270205+RafaZamoraUPIITA@users.noreply.github.com>
2022-06-21 10:24:01 -0500

    Update Europ.sql

--- Fragmentacion/Europ.sql
+++ Fragmentacion/Europ.sql
@@ -1,24 +1,35 @@
-CREATE DATABASE Europe
+Use Europe
+/*
+CREATE DATABASE Europa_Pacifico
 GO
 
-use Europe
-GO
+use Europa_Pacifico
+GO*/
+--SE USÓ PARA REPLICAR LAS TABLAS NECESARIAS PARA LAS CONSULTAS EN LOS FRAGMENTOS DE NorteAmerica y Europe
+create schema Production 
 create schema Sales
-create schema Production
+--REPLICA DE SpecialOffer
+SELECT so.* INTO Sales.SpecialOffer
+FROM AdventureWorks2019.Sales.SpecialOffer so
+go
+--REPLICA DE SpecialOfferProduct
+SELECT sop.* INTO Sales.SpecialOfferProduct
+FROM AdventureWorks2019.Sales.SpecialOfferProduct sop
+go
+--REPLICA DE SalesPerson
+SELECT sp.* INTO Sales.SalesPerson
+FROM AdventureWorks2019.Sales.SalesPerson sp
+go
+--REPLICA DE Product
+SELECT p.* INTO Production.Product
+FROM AdventureWorks2019.Production.Product p
+go
+--REPLICA DE ProductCategory
+SELECT pc.* INTO Production.ProductCategory
+FROM AdventureWorks2019.Production.ProductCategory pc
+go
+--REPLICA DE ProductSubcategory
+SELECT psc.* INTO Production.ProductSubcategory
+FROM AdventureWorks2019.Production.ProductSubcategory psc
+go
 
-SELECT c.* INTO Sales.Customer
-FROM AdventureWorks2019.Sales.Customer c
-where TerritoryID IN (7,8,10)
-
-SELECT ORH.* INTO Sales.SalesOrderHeader
-FROM AdventureWorks2019.Sales.SalesOrderHeader ORH
-WHERE TerritoryID in (SELECT TerritoryID
-						FROM AdventureWorks2019.Sales.SalesTerritory
-						WHERE [Group]='EUROPE') 
-
---Fragmento SalesOrderDetail Norteamerica
-SELECT DISTINCT sod.* INTO Sales.SalesOrderDetail
-FROM AdventureWorks2019.Sales.SalesOrderDetail sod
-JOIN (SELECT * FROM Europe.Sales.SalesOrderHeader) st
-ON sod.SalesOrderID = st.SalesOrderID
-GO
